4f16cfa79fd2aaf52547c829fec44c998ddb95c1
Dugbazah Redeemer <dugbazahredeemer@gmail.com>
2023-03-28 21:06:21 +0000

    changes made on nave-bar

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
 let menuBtn = document.querySelector(".menu__bar");
+let menuBtn2 = document.querySelector(".menu__bar2");
 let listContainer = document.querySelector(".for__phone");
 let myList = false;
 listContainer.style.left = "1150px";
 
 function menuSection() {
     if (myList == false) {
-        listContainer.style.left = "0"; 
+        listContainer.style.left = "0";
+        menuBtn.style.opacity = "0";
+        menuBtn2.style.opacity = "1";
         myList = true;
     }
     else if (myList == true) {
-        listContainer.style.left = "1150px"; 
+        listContainer.style.left = "1150px";
+        menuBtn.style.opacity = "1";
+        menuBtn2.style.opacity = "0";
+        listContainer.style.transition = "0.3s ease-out";
         myList = false;
     }
 }
 
 menuBtn.onclick = menuSection;
+menuBtn2.onclick = menuSection;
